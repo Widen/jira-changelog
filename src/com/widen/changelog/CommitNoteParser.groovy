37c34937ae02a3f29a08ea3a98f75e99db5b550c
Ray Nicholus <rnicholus@users.noreply.github.com>
2015-07-01 14:46:40 -0500

    fix(clone): cloning into temp dir may not function as expected DAM-2351

--- src/com/widen/changelog/CommitNoteParser.groovy
+++ src/com/widen/changelog/CommitNoteParser.groovy
@@ -105,7 +105,6 @@ class CommitNoteParser
     }
 
     String cloneRepo(String url) {
-        String tempDir = System.getProperty("java.io.tmpdir");
         def repoMatcher = url =~ /^(?:git@|https:\/\/).*\/(.+)\.git$/
 
         if (!repoMatcher) {
@@ -114,11 +113,11 @@ class CommitNoteParser
 
         String repoName = repoMatcher[0][1]
 
-        File tempRepo = new File(tempDir, repoName)
+        File tempRepo = new File(repoName)
         tempRepo.deleteDir()
 
         LOGGER.info("cloning $repoName...")
-        executeCmd("git clone $url", tempDir)
+        executeCmd("git clone $url")
         LOGGER.info("...cloned $repoName")
 
         return tempRepo.path
@@ -128,9 +127,16 @@ class CommitNoteParser
         return executeCmd("git --no-pager log --no-merges --pretty=format:HASH:%H%nAUTHOR:%an%nSUBJECT:%s%nBODY:%b%n$COMMIT_DELIMITER $lastTag..$firstTag", tempRepoLocation, COMMIT_DELIMITER)
     }
 
-    List<String> executeCmd(def cmd, String workingDir, String delim="\\n") {
+    List<String> executeCmd(def cmd, String workingDir=null, String delim="\\n") {
         LOGGER.info("Executing command: $cmd")
-        def proc = cmd.execute(null, new File(workingDir))
+        def proc
+
+        if (workingDir) {
+            proc = cmd.execute(null, new File(workingDir))
+        }
+        else {
+            proc = cmd.execute()
+        }
         return proc.getText().split(delim)
     }
 
